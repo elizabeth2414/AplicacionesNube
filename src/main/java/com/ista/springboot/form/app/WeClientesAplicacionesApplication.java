f72c00afcd4a197dcdf65099df361af703b6c8f0
Carmen Neira <carmen.neira.est@tecazuay.edu.ec>
2025-10-03 11:08:26 +0200

    Primer commit del proyecto

---
+++ src/main/java/com/ista/springboot/form/app/WeClientesAplicacionesApplication.java
@@ -0,0 +1,13 @@
+package com.ista.springboot.form.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeClientesAplicacionesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeClientesAplicacionesApplication.class, args);
+	}
+
+}
